bf1e9f8043edb53774afc4eac6bdac9f51488673
Guilherme Bittencourt <68746811+bittenks@users.noreply.github.com>
2021-01-11 13:46:12 -0300

    animation

--- script.js
+++ script.js
@@ -3,14 +3,30 @@ const container = document.getElementById("container");
 
 btn.addEventListener("click", () => {
     createNotification();
+    createNotification1()
 });
+
+function createNotification1() {
+    const notif = document.createElement("div");
+    notif.classList.add("toast");
+
+    notif.innerText =    "OI! EU SOU O SR. MEESEEKS! OLHEM PRA MIM! ";
+    notif.innerHTML = "<img src='https://i.imgur.com/cmNlHdA.gif' class='mark' >" 
+    
+    container.appendChild(notif);
+
+    setTimeout(() => {
+        notif.remove();
+    }, 3000);
+}
 
 function createNotification() {
     const notif = document.createElement("div");
     notif.classList.add("toast");
 
     notif.innerText = "OI! EU SOU O SR. MEESEEKS! OLHEM PRA MIM! ";
-
+     
+    
     container.appendChild(notif);
 
     setTimeout(() => {
